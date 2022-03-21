4169ea95bcaa4d3c547e2292278b38d1cd0773ce
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-21 20:05:54 -0300

    Añade componente 'presentation', limpieza codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { Header } from './components/Header/Header'
+import { Presentation } from './components/Presentation/Presentation'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Header/>
-      
+        <Header />
+        <Presentation/>
     </div>
   )
 }
