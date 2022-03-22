70183eacd60e8643a5156592a097628794fe1a73
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-21 23:17:05 -0300

    Añade info de habilidades, responsive

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Skills.css'
+import { CardsSkills } from '../CardsSkills/CardsSkills'
+
+export const Skills = () => {
+    return (
+        <div>
+            <div className="row">
+                <div className="col-12">
+                    <h2 className='text-center my-4'>
+                        Habilidades
+                    </h2>
+                </div>
+                <div className="col-12">
+                    <div className='text-center container-info-skills'>
+                        <p className='info-skills'>
+                            Soy graduado de <span className="span-DG">Digital House</span> , donde realicé
+                            el curso
+                            de Desarrollador Full Stack. En la misma, aprendí diferentes lenguajes y herramientas de
+                            programación. A
+                            continuación se muestra una descripcion general rápida de mis pincipales tecnicas y herramientas
+                            que
+                            utilizo.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <CardsSkills />
+        </div>
+    )
+}
