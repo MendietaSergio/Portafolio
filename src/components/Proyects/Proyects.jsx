980c1925b3013e662cd88ac83ace0d2fa0988223
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-22 21:39:06 -0300

    Añade banner de 'Proyects', donde tiene una breve descripcion

---
+++ src/components/Proyects/Proyects.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { ContainerProyects } from '../ContainerProyects/ContainerProyects'
+import './Proyects.css'
+export const Proyects = () => {
+    return (
+        <>
+            <div className='row row-proyects'>
+                <div className="col-12">
+                    <h1 className='title-proyects' >Proyectos</h1>
+                </div>
+                <div className="col-12">
+                    <p>En esta sección están algunos de los proyectos que voy
+                        realizando de forma autónoma. Algunos estan hechos de cursos que
+                        realicé o clonando páginas para ir mejorando la practica,
+                        implementando mejoras, ya sea visuales o funcionales.</p>
+                </div>
+            </div>
+            <ContainerProyects />
+        </>
+    )
+}
