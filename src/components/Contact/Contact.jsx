8c48daee83f370af7a39320b8f0621d98b90ed80
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-22 22:05:17 -0300

    Añade componente 'contact' con titulo contacto, falta completar el formulario

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Contact.css'
+export const Contact = () => {
+  return (
+    <>
+        <div className='row'>
+            <div className='container-contact d-flex'>
+                <h1>Contacto</h1>
+            </div>
+        </div>
+    </>
+  )
+}
