a67ee8c0f482c2428da4757528b319a71004ffdf
MendietaSergio <mendieta.sergio.1995@gmail.com>
2022-03-23 21:57:51 -0300

    Añade componente footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className='row row-footer'>
+            <div className='col-12 d-flex justify-content-center my-3'>
+                <small>Copyright © portafolio <a className="footer-a"
+                    href="http://linkedin.com/in/mendietasergio" target="_blank"
+                    rel="noopener noreferrer">Mendieta Sergio</a></small>
+            </div>
+            <div className="col-12">
+
+            </div>
+        </div>
+    )
+}
