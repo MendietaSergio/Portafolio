b4b13a7f149aa733bc1247c72590d24ef5a88ab9
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-15 16:50:35 -0600

    UPDATE: button filter proyect

--- src/components/ContainerProyects/ContainerProyects.jsx
+++ src/components/ContainerProyects/ContainerProyects.jsx
@@ -1,43 +1,96 @@
-import axios from 'axios'
-import React, { useState, useEffect } from 'react'
-import { URL } from '../../Constants/apiUrl'
-import { Skeleton } from '../Skeleton/Skeleton'
-import { CardsProyects } from './CardsProyects'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { URL } from "../../Constants/apiUrl";
+import { Skeleton } from "../Skeleton/Skeleton";
+import { CardsProyects } from "./CardsProyects";
 
 export const ContainerProyects = () => {
-    const [proyects, setProyects] = useState([])
-    const [loading, setLoading] = useState(true)
-    useEffect(() => {
-        const getProyects = async () => {
-            await axios.get(URL.main + 'api/proyects')
-                .then((res) => {
-                    setProyects(res.data.listProyects)
-                })
-                .catch((error) => console.log(error))
-                .finally(() => {
-                    setTimeout(() =>{
-                        setLoading(false)
-                    }, 2000)
-                })
+  const [proyects, setProyects] = useState();
+  const [loading, setLoading] = useState(true);
+  const [selectBtn, setSelectBtn] = useState("Todos");
+  const getProyects = async () => {
+    await axios
+      .get(URL.main + "api/proyects")
+      .then((res) => {
+        const { listProyects } = res.data;
+        if (selectBtn !== "Todos") {
+          setProyects(listProyects.filter((e) => e.type === selectBtn));
+        } else {
+          setProyects(listProyects);
         }
-        getProyects()
-    }, [])
+      })
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 2000);
+      });
+  };
+  useEffect(() => {
+    setLoading(true);
+    getProyects();
+  }, [selectBtn]);
 
-    return (
-        <div className='row row-proyects-cards'>
-            {loading ? (
-                <>
-                    <Skeleton />
-                </>
-            ) : (
-                proyects.map((proyect) => (
-                    <div key={proyect._id} className="col-12 col-md-4 col-lg-3 container-proyects">
-                        <CardsProyects  proyect={proyect} />
-                    </div>
-                ))
-            )}
+  return (
+    <div className="row row-proyects-cards">
+      <div className="col-12 d-flex justify-content-start my-4">
+        <div>
+          <button
+            className={`btn mx-3 ${
+              selectBtn === "Todos" ? "btn-info" : "btn-light"
+            }`}
+            onClick={() => setSelectBtn("Todos")}
+          >
+            Todos
+          </button>
         </div>
-    )
-}
-
+        <div>
+          <button
+            className={`btn mx-3 ${
+              selectBtn === "Freelance" ? "btn-info" : "btn-light"
+            }`}
+            onClick={() => setSelectBtn("Freelance")}
+          >
+            Freelance
+          </button>
+        </div>
+        <div>
+          <button
+            className={`btn mx-3 ${
+              selectBtn === "Challenge" ? "btn-info" : "btn-light"
+            }`}
+            onClick={() => setSelectBtn("Challenge")}
+          >
+            Challenge
+          </button>
+        </div>
+        <div>
+          <button
+            className={`btn mx-3 ${
+              selectBtn === "Practicas" ? "btn-info" : "btn-light"
+            }`}
+            onClick={() => setSelectBtn("Practicas")}
+          >
+            Practicas
+          </button>
+        </div>
+      </div>
+      {loading ? (
+        <>
+          <Skeleton />
+        </>
+      ) : (
+        <>
+          {proyects.map((proyect) => (
+            <div
+              key={proyect._id}
+              className="col-12 col-md-4 col-lg-3 container-proyects"
+            >
+              <CardsProyects proyect={proyect} />
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
