eb846f4afe3d9fcbefa72b69153d818f2c27817f
Sergio Mendieta <mendieta.sergio.1995@gmail.com>
2023-01-29 23:05:12 -0300

    ADD: google analytics, dotenv

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Proyects } from "./components/Proyects/Proyects";
 import { Skills } from "./components/Skills/Skills";
 import ReactGA from "react-ga";
 function App() {
-  ReactGA.initialize(G - J73Q2JFBHG);
+  ReactGA.initialize("G - J73Q2JFBHG");
   return (
     <div className="container-fluid">
       <Header />
